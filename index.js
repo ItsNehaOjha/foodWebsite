9c5227b842fb42ae435d2b3f77f2a05a8b41a57d
ItsNehaOjha <ojhaneha0028@gmail.com>
2024-12-02 18:05:44 +0530

    hamburger

--- index.js
+++ index.js
@@ -22,15 +22,22 @@ setInterval(()=>{
         count=0
     }
     myFun()
-},4000)
+},3000)
 
 
-
-
-
-
-
-
+const bar = document.getElementById('bar');
+const close = document.getElementById('close');
+const nav = document.getElementById('navbar');
+if(bar){
+    bar.addEventListener('click',()=>{
+        nav.classList.add('active');
+    })
+}
+if(close){
+    close.addEventListener('click',()=>{
+        nav.classList.remove('active');
+    })
+}
 
 // cart
 button.forEach((curBtn)=>{
